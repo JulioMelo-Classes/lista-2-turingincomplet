ecd385e3052ef5d92853be31bd7456cf45d87bd2
turingincomplet <81372195+turingincomplet@users.noreply.github.com>
2021-06-27 09:58:44 -0300

    find-C

--- find/include/find.h
+++ find/include/find.h
@@ -10,14 +10,24 @@ using std::sort;
 
 namespace graal {
 
-/*! 
- * TODO: documentação no estilo doxygen
+/**
+ * @tparam InputIt tipo de iterator
+ * @tparam T tipo do valor
+ * @tparam Equal tipo da função Equal
+ * @param first iterator que aponta para o inicio do range
+ * @param last iterator que aponta para o final do range
+ * @param value valor a ser comparado
+ * @param eq função que testa igualdade
+ * @return InputIt iterator para o primeiro elemento que satisfaz a função eq()
  */
 template<class InputIt, class T, class Equal>
 InputIt find(InputIt first, InputIt last, const T& value, Equal eq)
 {
-    // TODO
-    return first;
+    for(;first<last;first++){
+      if(eq(*first,value))
+        return first;
+    }
+    return last;
 }
 
 }
