4822586a019fcf916ffda39b55ad42f2204247e1
turingincomplet <81372195+turingincomplet@users.noreply.github.com>
2021-06-27 07:19:13 -0300

    minmax-C

--- minmax/include/minmax.h
+++ minmax/include/minmax.h
@@ -1,7 +1,3 @@
-#ifndef GRAAL_H
-#define GRAAL_H
-
-#include <utility>
 using std::pair;
 #include <iterator>
 using std::distance;
@@ -10,25 +6,34 @@ using std::sort;
 
 namespace graal {
 
-/*! 
- * Exemplo de documentação seguindo o formato Doxygen
- *
- * @tparam Itr iterator para o range.
- * @tparam Compare o tipo de uma função bool(const T &a, const T &b)
- *
- * @param first Ponteiro para o primeiro elemento do range
- * @param last Ponteiro para a posição logo após o último elemento do range
- * @param cmp A função de comparação que retorna true quando o primeiro parâmetro é menor do que o segundo
- *
- * @return Um std::pair contendo o menor e maior elemento, nesta ordem
- *
- */
-template <typename Itr, typename Compare >
-std::pair<Itr, Itr> minmax( Itr first, Itr last, Compare cmp )
-{
-    // TODO
-    return std::make_pair( first, first );
-}
+    /*!
+     * Exemplo de documentação seguindo o formato Doxygen
+     *
+     * @tparam Itr iterator para o range.
+     * @tparam Compare o tipo de uma função bool(const T &a, const T &b)
+     *
+     * @param first Ponteiro para o primeiro elemento do range
+     * @param last Ponteiro para a posição logo após o último elemento do range
+     * @param cmp A função de comparação que retorna true quando o primeiro parâmetro é menor do que o segundo
+     *
+     * @return Um std::pair contendo o menor e maior elemento, nesta ordem
+     *
+     */
+    template <typename Itr, typename Compare >
+    std::pair<Itr, Itr> minmax(Itr first, Itr last, Compare cmp)
+    {
+        auto maxi = first, mini = first;
+        for (; first!=last; first++) {
+            if (!cmp(*first, *maxi)) {
+                 maxi = first;
+            }
+            if (cmp(*first, *mini)) {
+                mini =first;
+            }
 
+        }
+        return std::make_pair(mini, maxi);
+    }
 }
 #endif
+
