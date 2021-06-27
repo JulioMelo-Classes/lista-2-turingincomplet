8dd36117f84ac8f28f21b489bbca0c23db61ad8a
turingincomplet <81372195+turingincomplet@users.noreply.github.com>
2021-06-27 17:05:23 -0300

    partition-C

--- partition/include/partition.h
+++ partition/include/partition.h
@@ -10,14 +10,25 @@ using std::sort;
 
 namespace graal {
 
-/*! 
- * TODO: documentação no estilo doxygen
+/**
+ * @tparam ForwardIt tipo de iterator
+ * @tparam UnaryPredicate tipo do predicado
+ * @param first iterator que aponta para o inicio do range
+ * @param last iterator que aponta para o final do range
+ * @param p o predicado
+ * @return ForwardIt Um iterator apontando para o logo após o último elemento no range reordenado que satisfaz o predicado
  */
 template<class ForwardIt, class UnaryPredicate>
 ForwardIt partition(ForwardIt first, ForwardIt last, UnaryPredicate p)
 {
-    // TODO
-    return last;
+  auto ref=first;
+  for(;ref<last;ref++){
+    if(p(*ref))
+        std::swap(*ref,*first);
+    if(p(*first))
+      first++; 
+  }
+    return first;
 }
 
 }
