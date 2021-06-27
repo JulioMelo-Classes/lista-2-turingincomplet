5ec2fb4e039194854c9dfb87a55d08cc3ab66e45
turingincomplet <81372195+turingincomplet@users.noreply.github.com>
2021-06-27 14:09:32 -0300

    unique-C

--- unique/include/unique.h
+++ unique/include/unique.h
@@ -10,14 +10,28 @@ using std::sort;
 
 namespace graal {
 
-/*! 
- * TODO: documentação no estilo doxygen
+/**
+ * @tparam InputIt tipo de iterator
+ * @tparam Equal tipo da função eq
+ * @param first iterator que aponta para o inicio do range
+ * @param last iterator que aponta para o fim do range
+ * @param eq função de teste de igualdade
+ * @return iterator que aponta para o fim do range processado
  */
 template<class InputIt, class Equal>
 InputIt unique( InputIt first, InputIt last, Equal eq )
 {
-    // TODO
-    return first;
+    auto vef=first;
+    for(;first<last;first++){
+      vef=first+1;
+      while(vef<last){
+        if(eq(*vef,*first)){
+          last=std::remove(vef,vef,*vef);
+        }
+        vef++;
+      }
+    }
+    return last;
 }
 
 }
