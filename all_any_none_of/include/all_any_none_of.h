23d69784c014807bd72bfb0d5d1ffb6edfdd45b3
turingincomplet <81372195+turingincomplet@users.noreply.github.com>
2021-06-27 10:47:19 -0300

    all_any_none_of-C

--- all_any_none_of/include/all_any_none_of.h
+++ all_any_none_of/include/all_any_none_of.h
@@ -1,5 +1,7 @@
 #ifndef GRAAL_H
 #define GRAAL_H
+#ifndef GRAAL_H
+#define GRAAL_H
 
 #include <utility>
 using std::pair;
@@ -10,34 +12,63 @@ using std::sort;
 
 namespace graal {
 
-/*! 
- * TODO: documentação no estilo doxygen
+/**
+ * @tparam InputIt tipo do iterator
+ * @tparam UnaryPredicate tipo da função predicado
+ * @param first iterator que aponta para o inicio do range
+ * @param last iterator que aponta para o final do range
+ * @param p função predicado
+ * @return true se todos os elementos satisfazerem o predicado
+ * @return false se tiver algum elemento que não satifaz o predicado
  */
 template<class InputIt, class UnaryPredicate>
 bool all_of(InputIt first, InputIt last, UnaryPredicate p)
 {
-    // TODO
+  if(first==last)
     return false;
+  for(;first<last;first++){
+    if(!p(*first))
+    return false;
+  }
+    return true;
 }
 
-/*! 
- * TODO: documentação no estilo doxygen
+/**
+ * @tparam InputIt tipo do iterator
+ * @tparam UnaryPredicate tipo da função predicado
+ * @param first iterator que aponta para o inicio do range
+ * @param last iterator que aponta para o final do range
+ * @param p função predicado
+ * @return true se tiver algum elemento que satifaz o predicado
+ * @return false se nenhum elemento satisfaz o predicado
  */
 template<class InputIt, class UnaryPredicate>
 bool any_of(InputIt first, InputIt last, UnaryPredicate p)
 {
-    // TODO
+    for(;first<last;first++){
+     if(p(*first))
+      return true;
+   }
     return false;
 }
 
-/*! 
- * TODO: documentação no estilo doxygen
+/**
+ * @tparam InputIt tipo do iterator
+ * @tparam UnaryPredicate tipo da função predicado
+ * @param first iterator que aponta para o inicio do range
+ * @param last iterator que aponta para o final do range
+ * @param p função predicado
+ * @return true se nenhum elemento satisfaz o predicado
+ * @return false se tiver algum elemento que satifaz o predicado
  */
 template<class InputIt, class UnaryPredicate>
 bool none_of(InputIt first, InputIt last, UnaryPredicate p)
 {
-    // TODO
-    return false;
+  for(;first<last;first++){
+     if(p(*first))
+      return false;
+   }
+    return true;
 }
 
 }
