e2fa5f4b595c81e10b66c17ef02693d520e5a3a4
turingincomplet <81372195+turingincomplet@users.noreply.github.com>
2021-06-27 08:44:11 -0300

    copy-C

--- copy/include/copy.h
+++ copy/include/copy.h
@@ -10,14 +10,21 @@ using std::sort;
 
 namespace graal {
 
-/*! 
- * TODO: documentação no estilo doxygen
+/**
+ * @tparam InputIt tipo de iterator
+ * @param first iterator que aponta para o inicio do input range
+ * @param last iterator que aponta para o final do input range
+ * @param d_first iterator que aponta para o inicio do output range
+ * @return InputIt iterator que aponta para o final do output range
  */
+
 template<class InputIt>
 InputIt copy(InputIt first, InputIt last, InputIt d_first)
 {
-    // TODO
-    return first;
+ for(;first<last;first++,d_first++)
+  *d_first=*first;
+    
+    return ++d_first;
 }
 
 }
